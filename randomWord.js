e166f5f6e7256332739525d9f47e51c21178b117
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-07 17:50:30 +0500

    formatting

--- randomWord.js
+++ randomWord.js
@@ -43,22 +43,22 @@ const words = ["Juda", "qiziqarli", "Zo'r", "gap", "Ajoyib", "maslahat", "Bu", "
     "juda", "muhim", "Bu", "yangilik", "meni", "hayratda", "qoldirdi", "Sizning", "maslahatlaringiz",
     "juda", "foydali", "Bu", "ishni", "amalga", "oshirish", "shart"];
 
-    // var sarcasticWords = [
-    //     "Juda qiziqarli", "Zo'r gap", "Ajoyib maslahat", "Bu ishni albatta qilish kerak", "Sen juda aqllisan", 
-    //     "Bu yangilikni kutgan edim", "Sen haqiqiy dahosansan", "Ajoyib yutuq", "Xo'p, men seni tinglayman", 
-    //     "Sen hamma narsani bilasan", "Albatta, bu muhim", "Juda foydali maslahat", "Bu haqiqatdan ham muhim", 
-    //     "Sen doimo to'g'ri gapirasan", "Bu eng yaxshi reja", "Buni eshitish juda ajoyib", "Bu juda kerak edi", 
-    //     "Sen mutlaqo to'g'ri aytasan", "Bu shunchaki fantastik", "Juda kerakli ma'lumot", "Siz eng yaxshisisiz", 
-    //     "Men sizning fikringizni kutgan edim", "Bu haqiqatan ham hayratlanarli", "Sizni eshitish baxt", 
-    //     "Siz haqiqiy mutaxassissiz", "Sizning maslahatlaringiz doimo foydali", "Bu ishni uddalash kerak", 
-    //     "Bu juda dolzarb", "Men bu haqida o'ylamagan edim", "Bu fikr ajoyib", "Siz haqiqatan ham juda zo'rsiz", 
-    //     "Bu yangilik meni hayratda qoldirdi", "Sizning fikringiz men uchun juda qadrli", "Bu juda kerakli maslahat", 
-    //     "Sizni eshitish juda yaxshi", "Bu juda ajoyib yangilik", "Bu juda foydali ma'lumot", "Sizning fikringizni eshitish men uchun baxt", 
-    //     "Bu maslahat meni qutqaradi", "Bu ishni amalga oshirish shart", "Siz juda iste'dodlisiz", "Bu yangilik meni xursand qildi", 
-    //     "Bu maslahat meni hayratda qoldirdi", "Sizning fikringiz juda muhim", "Bu ishni amalga oshirish juda muhim", 
-    //     "Bu yangilik meni hayratda qoldirdi", "Sizning maslahatlaringiz doimo foydali", "Bu ishni amalga oshirish shart"
-    // ];
-    
+// var words = [
+//     "Juda qiziqarli", "Zo'r gap", "Ajoyib maslahat", "Bu ishni albatta qilish kerak", "Sen juda aqllisan", 
+//     "Bu yangilikni kutgan edim", "Sen haqiqiy dahosansan", "Ajoyib yutuq", "Xo'p, men seni tinglayman", 
+//     "Sen hamma narsani bilasan", "Albatta, bu muhim", "Juda foydali maslahat", "Bu haqiqatdan ham muhim", 
+//     "Sen doimo to'g'ri gapirasan", "Bu eng yaxshi reja", "Buni eshitish juda ajoyib", "Bu juda kerak edi", 
+//     "Sen mutlaqo to'g'ri aytasan", "Bu shunchaki fantastik", "Juda kerakli ma'lumot", "Siz eng yaxshisisiz", 
+//     "Men sizning fikringizni kutgan edim", "Bu haqiqatan ham hayratlanarli", "Sizni eshitish baxt", 
+//     "Siz haqiqiy mutaxassissiz", "Sizning maslahatlaringiz doimo foydali", "Bu ishni uddalash kerak", 
+//     "Bu juda dolzarb", "Men bu haqida o'ylamagan edim", "Bu fikr ajoyib", "Siz haqiqatan ham juda zo'rsiz", 
+//     "Bu yangilik meni hayratda qoldirdi", "Sizning fikringiz men uchun juda qadrli", "Bu juda kerakli maslahat", 
+//     "Sizni eshitish juda yaxshi", "Bu juda ajoyib yangilik", "Bu juda foydali ma'lumot", "Sizning fikringizni eshitish men uchun baxt", 
+//     "Bu maslahat meni qutqaradi", "Bu ishni amalga oshirish shart", "Siz juda iste'dodlisiz", "Bu yangilik meni xursand qildi", 
+//     "Bu maslahat meni hayratda qoldirdi", "Sizning fikringiz juda muhim", "Bu ishni amalga oshirish juda muhim", 
+//     "Bu yangilik meni hayratda qoldirdi", "Sizning maslahatlaringiz doimo foydali", "Bu ishni amalga oshirish shart"
+// ];
+
 
 function generator() {
     const res = document.getElementById('output');
@@ -69,7 +69,7 @@ function generator() {
         for (let i = 0; i < 3; i++) {
             const randomIndex = Math.floor(Math.random() * words.length);
             randomWords.push(words[randomIndex]);
-        }   
+        }
         res.innerHTML = `
             <p>${userInput}</p>
             <p> ${randomWords.join(' ')}</p>
